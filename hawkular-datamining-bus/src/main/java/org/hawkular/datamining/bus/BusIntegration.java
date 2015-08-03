9512975286a0a8a69d7c57dd678f1f1217132433
Pavol Loffay <p.loffay@gmail.com>
2015-08-03 18:05:49 +0200

    soruce code init

---
+++ hawkular-datamining-bus/src/main/java/org/hawkular/datamining/bus/BusIntegration.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.datamining.bus;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+import javax.jms.JMSException;
+import org.hawkular.bus.common.ConnectionContextFactory;
+import org.hawkular.bus.common.Endpoint;
+import org.hawkular.bus.common.MessageProcessor;
+import org.hawkular.bus.common.consumer.ConsumerConnectionContext;
+import org.hawkular.datamining.bus.listener.MetricDataListener;
+import org.jboss.logging.Logger;
+
+/**
+ * Created by pavol on 29.7.2015.
+ */
+public class BusIntegration {
+
+    private static final Logger LOG = Logger.getLogger(BusIntegration.class);
+
+    @Official
+    @Produces
+    @Singleton
+    public MetricDataListener getMetricDataListener() {
+        LOG.info("Initializing bus");
+        System.out.println("XXXInitializing bus\n\n");
+
+        MetricDataListener metricDataListener = null;
+        try {
+            ConnectionContextFactory factory = new ConnectionContextFactory(Configuration.BROKER_URL);
+
+            Endpoint ENDPOINT = new Endpoint(Endpoint.Type.TOPIC, Configuration.TOPIC_METRIC_DATA);
+            ConsumerConnectionContext context = factory.createConsumerConnectionContext(ENDPOINT);
+
+            MessageProcessor processor = new MessageProcessor();
+            metricDataListener = new MetricDataListener();
+            processor.listen(context, metricDataListener);
+        } catch (JMSException ex)  {
+            ex.printStackTrace();
+        }
+
+        return metricDataListener;
+    }
+}
---
+++ hawkular-datamining-dist/src/main/java/org/hawkular/datamining/dist/DataMiningStartup.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.datamining.dist;
+
+import org.hawkular.datamining.bus.Official;
+import org.hawkular.datamining.bus.listener.MetricDataListener;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import org.jboss.logging.Logger;
+
+/**
+ * Created by pavol on 1.8.2015.
+ */
+@Startup
+@Singleton
+public class DataMiningStartup {
+
+    private static final Logger LOG = Logger.getLogger(DataMiningStartup.class);
+
+    @Official
+    @Inject
+    private MetricDataListener metricDataListener;
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.debug("Ejb starting");
+    }
+}
